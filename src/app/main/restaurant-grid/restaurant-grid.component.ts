88595c4e33c18dbde9f98201c4397214a6446808
herunlei <gilgames.fate@gmail.com>
2018-06-30 12:19:31 -0400

    EH-41 can get current location and center

--- src/app/main/restaurant-grid/restaurant-grid.component.ts
+++ src/app/main/restaurant-grid/restaurant-grid.component.ts
@@ -14,121 +14,138 @@ const APP = environment.APP;
   styleUrls: ['./restaurant-grid.component.scss']
 })
 export class RestaurantGridComponent implements OnInit {
-    keyword:string;
-    query:any;
-    filter:any;
-    restaurantList:Restaurant[];
-    places:any[] = [];
-    center:any = {lat:43.761539,lng:-79.411079};
+  keyword: string;
+  query: any;
+  filter: any;
+  restaurantList: Restaurant[];
+  places: any[] = [];
+  center: any = { lat: 43.761539, lng: -79.411079 };
 
-    ngOnInit() {
-        // if(window.navigator.geolocation){
-        //     window.navigator.geolocation.getCurrentPosition(pos=>{
-        //       let c = pos.coords;
-        //       self.center = {lat:c.latitude, lng:c.longitude};
-        //     }, 
-        //     err=>{
-        //       self.center = {lat: 43.7823332, lng: -79.392994};
-        //     });
-        // }
-    }
-    ngAfterViewInit(){
-      let self = this;
-      
-    }
-    constructor(private commerceServ:CommerceService, private sharedServ:SharedService) {
-        let self = this;
+  ngOnInit() {
+    // if(window.navigator.geolocation){
+    //     window.navigator.geolocation.getCurrentPosition(pos=>{
+    //       let c = pos.coords;
+    //       self.center = {lat:c.latitude, lng:c.longitude};
+    //     }, 
+    //     err=>{
+    //       self.center = {lat: 43.7823332, lng: -79.392994};
+    //     });
+    // }
+  }
+  ngAfterViewInit() {
+    let self = this;
 
-        //get user's location
-        let s:string = localStorage.getItem('location-'+APP);
-        let loc = JSON.parse(s);
-        
-        if(loc){
-          self.center = {lat:loc.lat, lng:loc.lng};
-        }else{
-          self.center = {lat:43.761539,lng:-79.411079}; // default
+  }
+  constructor(private commerceServ: CommerceService, private sharedServ: SharedService) {
+    let self = this;
+
+    //get user's location
+    let lat;
+    let lng;
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        lat = +pos.coords.latitude;
+        lng = +pos.coords.longitude;
+        if (lat && lng !== 'undefind') {
+          self.center = { lat: lat, lng: lng };
+        } else {
+          self.center = { lat: 43.761539, lng: -79.411079 }; // default
         }
+      });
+    }
 
-        // setup event listener
-        this.sharedServ.getMsg().subscribe(msg => {
-            if('OnSearch' === msg.name){
-                if(msg.query){
-                  self.filter = msg.query;
-                  let query = {...self.filter, ...self.query};
-                  self.doSearchRestaurants(query);
-                }else{
-                    self.doSearchRestaurants(self.query.keyword);
-                }
-            }
+    /*
+    let s:string = localStorage.getItem('location-'+APP);
+    let loc = JSON.parse(s);
+    
+    if(loc){
+      self.center = {lat:loc.lat, lng:loc.lng};
+    }else{
+      self.center = {lat:43.761539,lng:-79.411079}; // default
+    }
+    */
+
+    // setup event listener
+    this.sharedServ.getMsg().subscribe(msg => {
+      if ('OnSearch' === msg.name) {
+        if (msg.query) {
+          self.filter = msg.query;
+          let query = { ...self.filter, ...self.query };
+          self.doSearchRestaurants(query);
+        } else {
+          self.doSearchRestaurants(self.query.keyword);
+        }
+      }
+    });
+
+    self.doSearchRestaurants(self.center);
+  }
+
+  searchByKeyword(keyword: string) {
+    let self = this;
+    this.query = { 'keyword': keyword };
+    let query = { ...self.filter, ...self.query };
+    self.doSearchRestaurants(query);
+  }
+
+
+  getFilter(query?: any) {
+    let qs = [];
+
+    if (query.categories && query.categories.length > 0) {
+      let s = query.categories.join(',');
+      qs.push('cats=' + s);
+    }
+
+    // if(query.restaurants && query.restaurants.length>0){
+    //   let s = query.restaurants.join(',');
+    //   qs.push('ms=' + s);
+    // }
+
+    // if(query.colors && query.colors.length>0){
+    //   let s = query.colors.join(',');
+    //   qs.push('colors=' + s);
+    // }
+    return qs;
+  }
+
+  doSearchRestaurants(query?: any) {
+    //query --- eg. {}
+    let self = this;
+    let qs = self.getFilter(query);
+    let s = '';
+    let conditions = [];
+
+    if (qs.length > 0) {
+      conditions.push(qs.join('&'));
+    }
+    if (query && query.keyword) {
+      conditions.push('keyword=' + query.keyword);
+    }
+    if (query && query.lat && query.lng) {
+      conditions.push('lat=' + query.lat + '&lng=' + query.lng);
+    }
+
+    if (conditions.length > 0) {
+      s = '?' + conditions.join('&');
+    }
+
+    self.commerceServ.getRestaurantList(s).subscribe(
+      (ps: Restaurant[]) => {
+        self.restaurantList = ps;//self.toProductGrid(data);
+        let a = [];
+        ps.map(restaurant => {
+          a.push({
+            lat: parseFloat(restaurant.address.lat),
+            lng: parseFloat(restaurant.address.lng),
+            name: restaurant.name
+          })
         });
-
-        self.doSearchRestaurants(self.center);
-    }
-  
-    searchByKeyword(keyword:string){
-      let self = this;
-      this.query = {'keyword': keyword};
-      let query = {...self.filter, ...self.query};
-      self.doSearchRestaurants(query);
-    }
-
-
-    getFilter(query?:any){
-      let qs = [];
-
-      if(query.categories && query.categories.length>0){
-        let s = query.categories.join(',');
-        qs.push('cats=' + s);
+        self.places = a;
+      },
+      (err: any) => {
+        self.restaurantList = [];
       }
-
-      // if(query.restaurants && query.restaurants.length>0){
-      //   let s = query.restaurants.join(',');
-      //   qs.push('ms=' + s);
-      // }
-
-      // if(query.colors && query.colors.length>0){
-      //   let s = query.colors.join(',');
-      //   qs.push('colors=' + s);
-      // }
-      return qs;
-    }
-
-    doSearchRestaurants(query?:any){
-        //query --- eg. {}
-        let self = this;
-        let qs = self.getFilter(query);
-        let s = '';
-        let conditions = [];
-
-        if(qs.length>0){
-          conditions.push(qs.join('&'));
-        }
-        if(query && query.keyword){
-          conditions.push('keyword=' + query.keyword);
-        }
-        if(query && query.lat && query.lng){
-          conditions.push('lat=' + query.lat + '&lng=' + query.lng);
-        }
-
-        if(conditions.length>0){
-          s = '?' + conditions.join('&');
-        }
-
-        self.commerceServ.getRestaurantList(s).subscribe(
-            (ps:Restaurant[]) => {
-                self.restaurantList = ps;//self.toProductGrid(data);
-                let a = [];
-                ps.map(restaurant=>{ 
-                  a.push({lat:parseFloat(restaurant.address.lat), 
-                    lng:parseFloat(restaurant.address.lng),
-                    name:restaurant.name
-                  }) 
-                });
-                self.places = a;
-            },
-            (err:any) => {
-                self.restaurantList = [];
-            }
-        );
-      }
+    );
+  }
 }
