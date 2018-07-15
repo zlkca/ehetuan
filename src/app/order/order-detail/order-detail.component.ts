0a9497a6d62c72f4bbbc222eb6fc54c63a13605d
Songqing Sun <sunsq1991@gmail.com>
2018-07-15 10:26:17 -0400

    order

--- src/app/order/order-detail/order-detail.component.ts
+++ src/app/order/order-detail/order-detail.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { OrderService } from '../order.service';
+import { Order } from '../order';
 
 @Component({
-  selector: 'app-order-detail',
-  templateUrl: './order-detail.component.html',
-  styleUrls: ['./order-detail.component.scss']
+    selector: 'app-order-detail',
+    templateUrl: './order-detail.component.html',
+    styleUrls: ['./order-detail.component.scss']
 })
 export class OrderDetailComponent implements OnInit {
+    order: Order;
 
-  constructor() { }
+    @Input() orderId: number;
 
-  ngOnInit() {
-  }
+    constructor(
+        private orderServ: OrderService,
+    ) { }
+
+    ngOnInit() {
+        this.orderServ.getOrder(this.orderId)
+            .subscribe((data: Order) => {
+                this.order = data;
+            });
+    }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -9,6 +9,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Order } from './order';
 
 const APP = environment.APP;
 const API_URL = environment.API_URL;
@@ -23,7 +24,7 @@ export class OrderService {
 
     constructor(private http: HttpClient) { }
 
-    checkout(orders: any, user_id: string): Observable<boolean> {
+    checkout(orders: any, user_id: string): Promise<Order> {
         const url = this.API_URL + 'orders';
 
         return this.http.post(url, {
@@ -32,12 +33,43 @@ export class OrderService {
             'user_id': user_id
             // 'created': d.created,
             // 'updated': d.updated,
-        }).pipe(map((res: any) => {
-            return res.success;
+        })
+        .toPromise()
+        .then((res: any) => {
+            if (res.success) {
+                return res.order;
+            } else {
+                throw new Error('order create failed');
+            }
+        });
+    }
+
+    getOrderList(query?:string):Observable<Order[]>{
+        const url = this.API_URL + 'orders' + (query ? query:'');
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.get(url, {'headers': headers}).pipe(map((res:any) => {
+            let a:Order[] = [];
+            if( res.data && res.data.length > 0){
+                for(let b of res.data){
+                    a.push(new Order(b));
+                }
+            }
+            return a;
         }),
-            catchError((err) => {
-                return observableThrowError(err.message || err);
-            }));
+        catchError((err) => {
+            return observableThrowError(err.message || err);
+        }),);
+    }
+
+    getOrder(id:number):Observable<Order>{
+        const url = this.API_URL + 'orders/' + id;
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.get(url, {'headers': headers}).pipe(map((res:any) => {
+            return new Order(res.data);
+        }),
+        catchError((err) => {
+            return observableThrowError(err.message || err);
+        }),);
     }
 
 }
